b32195f9c81fac998e942893ccb6747638609bf5
Krish6106 <krishkumarrana123@gmail.com>
2026-08-25 21:26:37 +0530

    Time: 4 ms (14.13%) | Memory: 10 MB (53.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-
     bool IsAlphanum(char ch){
         if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')) {
             return true;
         }
-        
+
         return false;
     }
-
-
     bool isPalindrome(string s) {
         int st = 0, end = s.length()-1;
         while(st<end){
@@ -21,7 +18,6 @@ public:
                 end--;
                 continue;
             }
-
             if(tolower(s[st])!=tolower(s[end])){
                 return false;
             }
